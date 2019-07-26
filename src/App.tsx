f4e39e9e1e1f757818a656c45afad92599a2464b
Vitorhpx <vitorhugoperles@gmail.com>
2019-07-26 19:55:14 -0300

    feat(global theme and app): global theme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,51 @@
-import React, {Component} from 'react';
-import NavBar from './Screens/NavBar/NavBar';
-import UserPanel from './Screens/UserPanel/UserPanel';
-import Portfolio from './Screens/Portfolio/Portfolio';
-import History from './Screens/History/History';
-import Marketplace from './Screens/Marketplace/Marketplace';
-import Login from './Screens/Login/Login';
-import SignUp from './Screens/SignUp/SignUp';
-import './App.css';
+import React, { Component } from "react";
+import NavBar from "./Screens/NavBar/NavBar";
+import UserPanel from "./Screens/UserPanel/UserPanel";
+import Portfolio from "./Screens/Portfolio/Portfolio";
+import History from "./Screens/History/History";
+import Marketplace from "./Screens/Marketplace/Marketplace";
+import Login from "./Screens/Login/Login";
+import SignUp from "./Screens/SignUp/SignUp";
+import "./App.css";
+import GlobalThemeProvider from "./components/global-theme-provider/global-theme-provider.component";
+import { Button } from "@material-ui/core";
 
-export enum AppScreen { USER_PANEL, PORTFOLIO, HISTORY, MARKETPLACE }
-
-type AppState = {
-	activeTab: AppScreen
+export enum AppScreen {
+  USER_PANEL,
+  PORTFOLIO,
+  HISTORY,
+  MARKETPLACE
 }
 
-class App extends Component <{}, AppState> {
-    constructor(props: object){
-		super(props);
-		this.changeTab = this.changeTab.bind(this);
-		this.state = {
-			activeTab: AppScreen.USER_PANEL
-		}
-	}
-	
-	changeTab(tab: AppScreen) {
-		this.setState({activeTab: tab});
-	}
+type AppState = {
+  activeTab: AppScreen;
+};
 
-    render(){
-        return(
-            <>
-				<NavBar
-					activeTab={this.state.activeTab}
-					changeTab={this.changeTab}
-				/>
-				<Login/>
-				{this.state.activeTab === AppScreen.USER_PANEL &&
-					<UserPanel/>
-				}{this.state.activeTab === AppScreen.PORTFOLIO &&
-					<Portfolio/>
-				}{this.state.activeTab === AppScreen.HISTORY &&
-					<History/>
-				}{this.state.activeTab === AppScreen.MARKETPLACE &&
-					<Marketplace/>
-				}
-			</>
-        )
-    }
+class App extends Component<{}, AppState> {
+  constructor(props: object) {
+    super(props);
+    this.changeTab = this.changeTab.bind(this);
+    this.state = {
+      activeTab: AppScreen.USER_PANEL
+    };
+  }
+
+  changeTab(tab: AppScreen) {
+    this.setState({ activeTab: tab });
+  }
+
+  render() {
+    return (
+      <>
+        <NavBar activeTab={this.state.activeTab} changeTab={this.changeTab} />
+        <Login />
+        {this.state.activeTab === AppScreen.USER_PANEL && <UserPanel />}
+        {this.state.activeTab === AppScreen.PORTFOLIO && <Portfolio />}
+        {this.state.activeTab === AppScreen.HISTORY && <History />}
+        {this.state.activeTab === AppScreen.MARKETPLACE && <Marketplace />}
+      </>
+    );
+  }
 }
 
 export default App;
